15bcd8adf65e6f07fda859e969c30f9a5cdfb1cd
Daniel Jaramillo <daniel.jaramilloa7@gmail.com>
2023-05-31 21:23:49 -0500

    Remove fixed lights on voice commands screen

--- lib/screens/voice.screen.dart
+++ lib/screens/voice.screen.dart
@@ -1,10 +1,13 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:izzy_casa_app/extensions/iterable_insert_between.dart';
+import 'package:izzy_casa_app/providers/lights.provider.dart';
+import 'package:provider/provider.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:izzy_casa_app/ui/voice/voice_cards.dart';
 
 class VoiceScreen extends StatefulWidget {
-  VoiceScreen({
+  const VoiceScreen({
     super.key,
   });
 
@@ -15,93 +18,86 @@ class VoiceScreen extends StatefulWidget {
 class _VoiceScreenState extends State<VoiceScreen> {
   var text = "Presiona el botón y comienza a hablar";
   var isListening = false;
-  bool statusPrincipal = false;
-  bool statusLivingroom = true;
   SpeechToText speechToText = SpeechToText();
 
   @override
   Widget build(BuildContext context) {
-    String imageOff = "assets/images/home/light-off.png";
-    String imageON = "assets/images/home/light-on.png";
-    String imagePrincipal = statusPrincipal ? imageON : imageOff;
-    String imageLivingroom = statusLivingroom ? imageON : imageOff;
+    var lights = context.watch<LightsProvider>().lights;
     return Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: AvatarGlow(
-          endRadius: 75.0,
-          animate: isListening,
-          duration: Duration(milliseconds: 2000),
-          glowColor: Colors.orange,
-          repeat: true,
-          repeatPauseDuration: Duration(milliseconds: 100),
-          showTwoGlows: true,
-          child: GestureDetector(
-            onTapDown: (details) async {
-              if (!isListening) {
-                var available = await speechToText.initialize();
-                if (available) {
-                  setState(() {
-                    isListening = true;
-                    speechToText.listen(onResult: (result) {
-                      setState(() {
-                        text = result.recognizedWords;
-                        if (text == "cocina encender") {
-                          print(text);
-                        }
-                      });
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75.0,
+        animate: isListening,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: Colors.orange,
+        repeat: true,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(onResult: (result) {
+                    setState(() {
+                      text = result.recognizedWords;
+                      if (text == "cocina encender") {
+                        print(text);
+                      }
                     });
                   });
-                }
+                });
               }
-            },
-            onTapUp: (details) {
-              setState(() {
-                isListening = false;
-              });
-              speechToText.stop();
-            },
-            child: CircleAvatar(
-              backgroundColor: Colors.orange,
-              radius: 35,
-              child: Icon(
-                isListening ? Icons.mic : Icons.mic_none,
-                color: Colors.white,
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.orange,
+            radius: 35,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Comando de voz'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Column(
+                children: [
+                  ...lights
+                      .map<Widget>((light) => VoiceCards(light: light))
+                      .insertBetween(const SizedBox(height: 30))
+                      .toList(),
+                  const SizedBox(height: 50),
+                  Text(
+                    text,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 150),
+                ],
               ),
             ),
           ),
         ),
-        appBar: AppBar(
-          title: const Text('Comandos por voz'),
-        ),
-        body: SafeArea(
-            child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          margin: const EdgeInsets.only(bottom: 150),
-          child: Center(
-            child: Column(
-              children: [
-                VoiceCards(
-                  label: 'Principal',
-                  image: imagePrincipal,
-                ),
-                const SizedBox(height: 20),
-                VoiceCards(
-                  label: 'Sala',
-                  image: imageLivingroom,
-                ),
-                const SizedBox(height: 50),
-                Text(
-                  text,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w300,
-                  ),
-                )
-              ],
-            ),
-          ),
-        )));
+      ),
+    );
   }
 }
--- lib/ui/voice/voice_cards.dart
+++ lib/ui/voice/voice_cards.dart
@@ -1,51 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:izzy_casa_app/models/light.model.dart';
 
 class VoiceCards extends StatelessWidget {
-  const VoiceCards({super.key, required this.label, required this.image});
+  const VoiceCards({super.key, required this.light});
 
-  final String label;
-  final String image;
+  final Light light;
+
+  final String imageOff = "assets/images/home/light-off.png";
+  final String imageON = "assets/images/home/light-on.png";
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        height: 120,
-        decoration: BoxDecoration(
-          color: Colors.blueGrey.shade50,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: -10,
-              blurRadius: 10,
-              offset: const Offset(10, 10),
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                label,
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade50,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: -10,
+            blurRadius: 10,
+            offset: const Offset(10, 10),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Text(
+                light.label,
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 27,
                   fontWeight: FontWeight.bold,
                   color: Colors.black.withOpacity(0.55),
                 ),
               ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.fitHeight,
-                ),
+            ),
+            const SizedBox(width: 20),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Image.asset(
+                light.state ? imageON : imageOff,
+                fit: BoxFit.fitHeight,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
